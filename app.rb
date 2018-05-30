f8f9a048d86216030b5dbfe130cf332c6dbfec3a
ChristianAJohnson <chrisgochris@gmail.com>
2018-05-30 11:53:36 -0400

    setup showing who is currently logged in, also setup logout button

--- app.rb
+++ app.rb
@@ -12,31 +12,46 @@ def current_user
 end
 
 get "/display"  do
-@users = User.all
-erb :"users/display"	
+	@users = User.all
+	erb :"users/display"	
 end
 
 get "/feed" do
-@posts = Post.all
-erb :"users/feed"
+	@posts = Post.all
+	erb :"users/feed"
 end
 
 get "/signup" do
-erb :"users/signup"
+	erb :"users/signup"
 end
 
 get "/login" do
-erb :"users/login"
+	erb :"users/login"
 end
 
+
+#All my Posts are below
+
 post "/create" do
-User.create(name: params[:name], password: params[:password])
-redirect "/display"
+	User.create(name: params[:name], password: params[:password])
+	redirect "/display"
 end
 
 post "/login" do
-
+	user = User.where(name: params[:name]).first
+	if user.password = params[:password] && user.name = params[:name]
+		session[:user_id] = user.id
+		redirect "/feed"
+	else
+		redirect "/login"
+	end
 end
+
+post "/logout" do
+	session[:user_id] = nil
+	redirect "/login"
+end
+
 
 
 
