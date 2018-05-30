fc9a6619110e8616b9c0ff8ed027471dbfc2eccd
ChristianAJohnson <chrisgochris@gmail.com>
2018-05-29 20:36:01 -0400

    added data to the database for testing

--- app.rb
+++ app.rb
@@ -2,3 +2,17 @@ require 'sinatra'
 require 'sinatra/activerecord'  
 set :database, 'sqlite3:foodreviews.sqlite3'  
 require './models'  
+
+
+get "/display"  do
+
+@users = User.all
+
+erb :"users/display"	
+end
+
+
+get "/feed" do
+
+erb :"users/feed"
+end
